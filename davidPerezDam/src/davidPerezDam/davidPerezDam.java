abfde45c39262d89bd4dbed22e1f35ff5a7f91af
david <dperezp01@iesalbarregas.es>
2021-03-12 19:18:17 +0100

    "primer commit"

---
+++ davidPerezDam/src/davidPerezDam/davidPerezDam.java
@@ -0,0 +1,7 @@
+package davidPerezDam;
+
+public class davidPerezDam {
+	public static void main(String [] args) {
+		System.out.println("Proyecto git eclipse");
+	}
+}
---
+++ davidPerezDam/src/module-info.java
@@ -0,0 +1,2 @@
+module davidPerezDam {
+}
