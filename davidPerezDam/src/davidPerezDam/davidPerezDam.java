86213b3b3f8da11b8e308cefd01b516a93c7bd9b
david <dperezp01@iesalbarregas.es>
2021-03-15 04:21:38 +0100

    "Líneas en método main"

--- davidPerezDam/src/davidPerezDam/davidPerezDam.java
+++ davidPerezDam/src/davidPerezDam/davidPerezDam.java
@@ -4,5 +4,8 @@ public class davidPerezDam {
 	public static void main(String [] args) {
 		System.out.println("Proyecto git eclipse");
 		System.out.println("Proyecto git eclipse 2");
+		var a=4;
+		System.out.println(Math.pow(a, a));
+		
 	}
 }
